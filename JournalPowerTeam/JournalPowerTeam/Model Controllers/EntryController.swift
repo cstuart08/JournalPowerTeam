a1180efb50018c3db61a9322663d8b9e6e2caaa6
Maxwell Poffenbarger <maxpoffenbarger@gmail.com>
2021-02-10 09:48:38 -0700

    Created basic outline for EntryController. Waiting on model (Entry) to continue forward

---
+++ JournalPowerTeam/JournalPowerTeam/Model Controllers/EntryController.swift
@@ -0,0 +1,33 @@
+//
+//  EntryController.swift
+//  JournalPowerTeam
+//
+//  Created by Maxwell Poffenbarger on 2/10/21.
+//
+
+import Foundation
+
+class EntryController {
+    
+    //MARK: - Properties
+    static let sharedInstance = EntryController()
+    
+    
+    //MARK: - Methods
+    func createEntry() {
+        //Need properties to be created to I can build without errors 
+    }
+    
+    func updateEntry() {
+        
+    }
+    
+    func deleteEntry() {
+        //We need equatable for this to work.......
+    }
+    
+    
+    //MARK: - Persistence
+    
+    
+}//End of class
